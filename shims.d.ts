9e8a1f5e75c809ee96dc60d497fa9bb77f498326
zhuangjinan <zhuangjinan2018@gmail.com>
2022-05-31 12:57:03 +0800

    test env

--- shims.d.ts
+++ shims.d.ts
@@ -3,3 +3,10 @@ import type { AttributifyAttributes } from "@unocss/preset-attributify";
 declare module "@vue/runtime-dom" {
   interface HTMLAttributes extends AttributifyAttributes {}
 }
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
